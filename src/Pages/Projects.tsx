bb730fccec1273d7dec3f8ddb818aaa91739d0b6
miguel5967 <luis_miguel099@hotmail.com>
2022-09-23 22:22:04 +0100

    change the border works and install react-i18next

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -2,10 +2,10 @@ import state from '../Constants'
 
 function Projects() {
   return (
-    <div className="flex flex-col border-black border-b-2">
+    <div className="flex flex-col ">
       {state.projects.map((project, index) => {
         return (
-          <div key={index}>
+          <div className='border-black border-t-2' key={index}>
             <div>
               <h1>Description</h1>
             </div>
@@ -24,25 +24,6 @@ function Projects() {
           </div>
         );
       })}
-
-      {/* ------------------- */}
-      {/* <div className="border-black border-t-2">
-        <div className="self-center">
-          <h1>Future Project</h1>
-        </div>
-
-        <div className="flex justify-center items-center h-32 mb-10">
-          <div className="flex w-56">
-            <a target="_blank" href="https://infocasa.pt">
-              <img alt="Infocasa Logo" src={Projects.infocasaLogo} />
-            </a>
-          </div>
-
-          <div className="flex max-w-2xl ml-7">
-            <p className="">{Projects.Game.gameDescripton}</p>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 }
